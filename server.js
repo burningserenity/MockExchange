fde209e4d4a57095d08aa3dde76582b220171337
burningserenity <burningserenity@mailbit.io>
2018-02-07 13:29:56 -0500

    Added function to execute trades; currently sets trade.open to false and increments the balance of the purchase currency buy the trade amount. bugs may be present.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const axios = require('axios');
+const users = require('./models/User');
 const trades = require('./models/Trade');
 
 // Express configuration
@@ -21,25 +22,39 @@ app.use("/", userRoutes, curRoutes, tradeRoutes);
 mongoose.connect(`mongodb://localhost/exchange`, {useMongoClient: true});
 mongoose.Promise = Promise;
 mongoose.connection.on('error', (err) => {
-    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+  console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
 });
 
 async function getAllPrices() {
-    let p;
-    let prices = [];
-    const rates = [{buy: 'btc',sell: 'usd'}, {buy: 'ltc',sell: 'btc'}, {buy: 'eth',sell: 'btc'}, {buy: 'doge',sell: 'btc'}];
-    rates.forEach(rate => {
-      p = axios.get(`http://127.0.0.1:8080/api/currencies/${rate.buy}/${rate.sell}`);
-      prices.push(p);
-    });
-    let pricesArr = await Promise.all(prices);
-    trades.find().then(dbTrades => {
-      dbTrades.forEach(trade => {
-        if (trade.curr_bought === 'btc' && trade.sold_amount >= pricesArr[0].data) {
-          console.log('trade');
+  let p;
+  let prices = [];
+  const rates = [{buy: 'btc',sell: 'usd'}, {buy: 'ltc',sell: 'btc'}, {buy: 'eth',sell: 'btc'}, {buy: 'doge',sell: 'btc'}];
+  const currencies = ['btc', 'ltc', 'eth', 'doge', 'usd'];
+  rates.forEach(rate => {
+    p = axios.get(`http://127.0.0.1:8080/api/currencies/${rate.buy}/${rate.sell}`);
+    prices.push(p);
+  });
+  let pricesArr = await Promise.all(prices);
+  trades.find().then(dbTrades => {
+    const openTrades = dbTrades.filter(trade => trade.open);
+    openTrades.forEach(trade => {
+      currencies.forEach(currency => {
+        if (trade.curr_bought === currency && trade.sold_amount >= pricesArr.indexOf(currency)) {
+          console.log(`${trade.owner} bought ${currency}`);
+          const balance = `${currency}_balance`;
+          trade.update({$set: { "open": false }}).then(() => {
+            users.findOne({"_id": trade.owner}).then(user => {
+              user.update({
+                $set: {
+                  [balance]: user[balance] + parseFloat(trade.bought_amount)
+                }
+              }).then(transaction => console.log(JSON.stringify(transaction, null, 2)));
+            });          
+          });
         }
       });
     });
+  });
   /*
    *    db.trades.find().then(trades => {
    *        if (currency_price_in_trade === current_price_of_currency) {
@@ -47,8 +62,8 @@ async function getAllPrices() {
    *            set trade.open to false;
    *        }
    *    })
-    * */
-  };
+   * */
+};
 
 // Server connection
 app.listen(port, () => {
